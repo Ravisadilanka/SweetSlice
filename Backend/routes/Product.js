f078aab1512781ed132b808d7083cad6317c1f2d
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2024-06-23 11:04:59 +0530

    Created endpoint for getting all products

--- Backend/routes/Product.js
+++ Backend/routes/Product.js
@@ -1,26 +1,37 @@
 const express = require('express')
-const { Product, validate} = require('../models/Product')
+const { Product, validate } = require('../models/Product')
 const router = express.Router()
 const upload = require('../multer-config')
 
+// Create products
 router.post('/', upload.single('image'), async (req, res) => {
     try {
         const { error } = validate(req.body)
         if (error) return res.status(400).send(error.details[0].message)
-    
+
         if (!req.file) return res.status(400).send('Image file is required.')
-    
+
         const product = new Product({
             name: req.body.name,
             description: req.body.description,
             price: req.body.price,
             image: req.file.path  // Save image path to database
         })
-    
+
         await product.save()
         res.send(product)
     } catch (error) {
         res.status(500).send('Creating product: ' + error.message)
+    }
+})
+
+// Get all products
+router.get('/', async (req, res) => {
+    try {
+        const product = await Product.find()
+        res.send(product)
+    } catch (error) {
+        res.status(500).send('Server error')
     }
 })
 
