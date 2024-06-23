80f0b7e442e14c0c4575f09c17bcff1f7bcfd902
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2024-06-23 19:11:22 +0530

    Created endpoint for removing products from cart

--- Backend/routes/Cart.js
+++ Backend/routes/Cart.js
@@ -15,9 +15,15 @@ router.post('/', auth, async (req, res) => {
         const product = await Product.findById(req.body.productId)
         if (!product) return res.status(400).send('Invalid product.')
 
-        user.cart.push({ productId, quantity })
-        await user.save()
+        const cartItem = user.cart.find(item => item.productId == productId)
 
+        if (cartItem) {
+            cartItem.quantity += quantity
+        } else {
+            user.cart.push({ productId, quantity })
+        }
+
+        await user.save()
         res.send(user.cart)
     } catch (error) {
         res.status(500).send('Something went wrong.')
@@ -32,6 +38,28 @@ router.get('/', auth, async (req, res) => {
 
         res.send(user.cart)
     } catch(error) {
+        res.status(500).send('Something went wrong.')
+    }
+})
+
+// Remove products from cart
+router.delete('/:cartItemId', auth, async(req, res) => {
+    try {
+        const user = await User.findById(req.user._id)
+        if (!user) return res.status(400).send('Invalid user.')
+
+        const cartItemId = req.params.cartItemId
+        const itemIndex = user.cart.findIndex(item => item._id == cartItemId)
+
+        if (itemIndex === -1) return res.status(404).send('Item not found in cart.')
+
+        user.cart.splice(itemIndex, 1)
+
+        await user.save()
+
+        res.send(user.cart)
+    } catch (error) {
+        console.error(error.message);
         res.status(500).send('Something went wrong.')
     }
 })
